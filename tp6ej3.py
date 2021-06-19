046823033f3503410faf6c228b5c8a46d86f3de9
Red Crossbones <gastonnehuen@outlook.com>
2021-06-19 18:22:50 -0300

    +

---
+++ tp6ej3.py
@@ -0,0 +1,15 @@
+################
+# Martín René - @martinvilu
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+
+
+def principal():
+    pass
+
+if __name__ == "__main__":
+    principal()
+
